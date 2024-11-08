e392c4f8a8c0de3ae2347d14ac046a0ee5e84d53
Ismar Muslić <ismar.muslic@outlook.com>
2024-11-08 23:23:37 +0100

    Meni u mainu update-an

--- Grupa4_Tim1_KnjigaRecepata/Program.cs
+++ Grupa4_Tim1_KnjigaRecepata/Program.cs
@@ -18,7 +18,7 @@ namespace Grupa4_Tim1_KnjigaRecepata
             ReceptService rs = new ReceptService(baza, ss);
 
             // TODO: Implementacija klase "KnjigaRecepataService"
-            KnjigaRecepataService kr = new KnjigaRecepataService();
+            //KnjigaRecepataService kr = new KnjigaRecepataService();
             
             OcjenaService oc = new OcjenaService();
             ShoppingListaService sl = new ShoppingListaService(baza, ss);
@@ -33,66 +33,95 @@ namespace Grupa4_Tim1_KnjigaRecepata
             ocjene.Add(ocjena1);
 
             Recept recept1 = new Recept(1, "prvi", VrstaJela.PREDJELO, "...", 4, sastojci, KompleksnostPripreme.LAKO, ocjene);
-
-            Console.WriteLine("Dobrodošli u kuharicu!");
-            Console.WriteLine("Odaberite opciju: ");
-            Console.WriteLine("1. Pretraga recepata po nazivu");
-            Console.WriteLine("2. Pretraga recepata po ocjeni");
-            Console.WriteLine("3. Odabir vrste recepta");
-            Console.WriteLine("4. Dodavanje novog recepta");
-            Console.WriteLine("5. Pretraga sastojka po nazivu");
-            Console.WriteLine("6. Dodavanje novog sastojka");
             
-            int opcija = Convert.ToInt32(Console.ReadLine());
-            Meni:
-            switch(opcija) {
+
+            // TODO: Dodati opciju korisniku da se vrati na drugiMeni nakon što završi sa pretragom
+            
+            // NOTICE: Cijeli kod za meni je subject to change, ovo je osnovna napamet sklepana verzija.
+
+            // Da, koristio sam flagove i goto :P
+        prviMeni:
+            Console.WriteLine("\nDobrodošli u kuharicu!\n");
+            Console.WriteLine("Odaberite željenu knjigu: ");
+            Console.WriteLine("1. Slana jela");
+            Console.WriteLine("2. Deserti");
+            Console.WriteLine("\n0. Izlaz");
+            int prvaOpcija = Convert.ToInt32(Console.ReadLine());
+            
+            switch(prvaOpcija) {
                 case 1:
-                    Console.WriteLine("Unesite naziv recepta: ");
-                    string naziv = Console.ReadLine();
-                    // Treba da pronađe recept po nazivu i ispiše ga u konzoli ili 
-                    // da ispiše poruku da recept nije pronađen (moze biti i u obliku exceptiona),
-                    // a zatim da ponudi korisniku da unese novi naziv ili da odustane
-                    kr.pretraziPoNazivu(naziv);
-                    
+                    Console.WriteLine("\nOdabrali ste knjigu sa slanim jelima.\n");
+                    Console.WriteLine("1. Pretraga recepata po nazivu");
+                    Console.WriteLine("2. Pretraga recepata po ocjeni");
+                    Console.WriteLine("3. Odabir vrste recepta");
+                    Console.WriteLine("4. Dodavanje novog recepta");
+                    Console.WriteLine("5. Pretraga sastojka po nazivu");
+                    Console.WriteLine("6. Dodavanje novog sastojka");
+                    Console.WriteLine("\n0. Povratak na početni meni");
+
+                    int drugaOpcija = Convert.ToInt32(Console.ReadLine());
+        drugiMeni:
+                    switch (drugaOpcija) {
+                        case 1:
+                            Console.WriteLine("Unesite naziv recepta: ");
+                            string naziv = Convert.ToString(Console.ReadLine());
+                            // Treba da pronađe recept po nazivu i ispiše ga u konzoli ili 
+                            // da ispiše poruku da recept nije pronađen (moze biti i u obliku exceptiona),
+                            // a zatim da ponudi korisniku da unese novi naziv ili da odustane
+                            //kr.pretraziPoNazivu(naziv);
+
+                            break;
+                        case 2:
+                            Console.WriteLine("Unesite ocjenu recepta: ");
+                            int ocjena = Convert.ToInt32(Console.ReadLine());
+                            // Isto kao i za pretragu po nazivu
+                            //kr.pretraziPoOcjeni(ocjena);
+                            break;
+                        case 3:
+                            Console.WriteLine("Unesite vrstu jela: ");
+                            string vrstaJela = Console.ReadLine();
+                            // Isto kao i za pretragu po nazivu
+                            //kr.pretraziPoVrstiJela(vrstaJela);
+                            break;
+                        case 4:
+                            Console.WriteLine("Unesite podatke za novi recept: ");
+                            // Treba da omogući korisniku unos podataka za novi recept
+                            // i da ga doda u bazu
+                            //kr.dodajRecept();
+                            break;
+                        case 5:
+                            Console.WriteLine("Unesite naziv sastojka: ");
+                            string nazivSastojka = Console.ReadLine();
+                            // Isto kao i za pretragu po nazivu
+                            //s.pretraziPoNazivu(nazivSastojka);
+                            break;
+                        case 6:
+                            Console.WriteLine("Unesite podatke za novi sastojak: ");
+                            // Treba da omogući korisniku unos podataka za novi sastojak
+                            // i da ga doda u bazu
+                            //ss.dodajSastojak();
+                            break;
+                        case 0:
+                            Console.WriteLine("Povratak na početni meni");
+                            goto prviMeni;
+                        default:
+                            Console.WriteLine("Unijeli ste pogrešnu opciju!");
+                            goto drugiMeni;
+                    }
                     break;
                 case 2:
-                    Console.WriteLine("Unesite ocjenu recepta: ");
-                    int ocjena = Convert.ToInt32(Console.ReadLine());
-                    // Isto kao i za pretragu po nazivu
-                    kr.pretraziPoOcjeni(ocjena);
-                    break;
-                case 3:
-                    Console.WriteLine("Unesite vrstu jela: ");
-                    string vrstaJela = Console.ReadLine();
-                    // Isto kao i za pretragu po nazivu
-                    kr.pretraziPoVrstiJela(vrstaJela);
-                    break;
-                case 4:
-                    Console.WriteLine("Unesite podatke za novi recept: ");
-                    // Treba da omogući korisniku unos podataka za novi recept
-                    // i da ga doda u bazu
-                    kr.dodajRecept();
-                    break;
-                case 5:
-                    Console.WriteLine("Unesite naziv sastojka: ");
-                    string nazivSastojka = Console.ReadLine();
-                    // Isto kao i za pretragu po nazivu
-                    ss.pretraziPoNazivu(nazivSastojka);
-                    break;
-                case 6:
-                    Console.WriteLine("Unesite podatke za novi sastojak: ");
-                    // Treba da omogući korisniku unos podataka za novi sastojak
-                    // i da ga doda u bazu
-                    ss.dodajSastojak();
+                    Console.WriteLine("Odabrali ste knjigu sa desertima.");
                     break;
                 default:
-                    Console.WriteLine("Unijeli ste pogrešnu opciju!");
-                    goto Meni;
+                    Console.WriteLine("Izlaz iz kuharice.");
+                    return;
             }
 
             
 
+            
 
+            // TODO: Dodati u svaki case gdje se prikazuje recept nakon prikaza, maybe izdvojiti u posebnu metodu
             /* ---- Ovaj dio moze ostati za konzolnu ---- */
             Console.WriteLine("Želite li ocijeniti odabrani recept (DA/NE): ");
             string unos = Console.ReadLine();
